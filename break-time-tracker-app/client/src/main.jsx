d8d4d736f5e94e55514571c6ec6c18bac2c18375
abhijittamang69-dev <abhijittamang69@gmail.com>
2026-08-25 00:03:15 +0300

    fix: MemoryRouter -> BrowserRouter for production + add vercel.json SPA routing

--- break-time-tracker-app/client/src/main.jsx
+++ break-time-tracker-app/client/src/main.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { MemoryRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MemoryRouter>
+    <BrowserRouter>
       <AuthProvider>
         <App />
       </AuthProvider>
-    </MemoryRouter>
+    </BrowserRouter>
   </React.StrictMode>,
 )
